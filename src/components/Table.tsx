1fef42d83185e6aa13fdba8761a3c1294965efa4
altug engin <altugengin@yahoo.com>
2025-09-04 21:56:24 +0300

    editable grid added

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -2,9 +2,10 @@ import useTableData from "./useTableData";
 import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
 import { type Item } from "./useTableData";
 
-import {DataGrid} from "@mui/x-data-grid"
-import { type GridColDef,type GridRowsProp } from "@mui/x-data-grid";
+import { DataGrid } from "@mui/x-data-grid";
+import { type GridColDef, type GridRowsProp } from "@mui/x-data-grid";
 import { useState } from "react";
+import FullFeaturedCrudGrid from "./FullFeaturedCrudGrid";
 
 export default function Table() {
   const { columns, data } = useTableData();
@@ -13,31 +14,26 @@ export default function Table() {
     columns,
     getCoreRowModel: getCoreRowModel(),
   });
-  
 
-const column=table.getHeaderGroups().map(item=>item.headers.map(item=>item.column.columnDef.header));
-const MUIcolumn: GridColDef[] = [];
-const [MUIdata]=useState<GridRowsProp>(data)
-column[0].map((item)=>MUIcolumn.push({
-  headerName: item?.toString(), width: 200,
-  //@ts-expect-error field type unknown
-  field: item?.toString()
-}))
-//data.map((item)=>MUIdata.push({id:item.id,Title:item.title,Supplier:item.supplier}))
-
-
-
+  const column = table
+    .getHeaderGroups()
+    .map((item) => item.headers.map((item) => item.column.columnDef.header));
+  const MUIcolumn: GridColDef[] = [];
+  const [MUIdata] = useState<GridRowsProp>(data);
+  column[0].map((item) =>
+    MUIcolumn.push({
+      headerName: item?.toString(),
+      width: 200,
+      //@ts-expect-error field type unknown
+      field: item?.toString(),
+    })
+  );
+  //data.map((item)=>MUIdata.push({id:item.id,Title:item.title,Supplier:item.supplier}))
 
   return (
-
-
-
-
-<DataGrid columns={MUIcolumn} rows={MUIdata}></DataGrid>
-
-
-
-
-  )
+    <>
+      <DataGrid columns={MUIcolumn} rows={MUIdata}></DataGrid>
+      <FullFeaturedCrudGrid></FullFeaturedCrudGrid>
+    </>
+  );
 }
-
